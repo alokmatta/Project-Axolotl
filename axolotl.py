a007d9738edae19649b7c07fb13bf79dc71bf876
Bernard Maltais <bernard.maltais@canada.ca>
2021-05-29 20:44:03 -0400

    Update backstorry for better results.

--- axolotl.py
+++ axolotl.py
@@ -36,8 +36,8 @@ def index():
   
     start_sequence = "\nAI:"
     restart_sequence = "\nHuman: "
-    prompt_init = "The following is a conversation with an AI animal. The animal is helpful, creative, clever, and very friendly. His name is BЯYAN. He is a salamander axolotl. He is 9 months old."
-    prompt_backstorry = ""
+    prompt_init = "The following is a conversation with an AI animal. The animal is helpful, funny, creative, clever, and very friendly. His name is BЯYAN. He is a salamander axolotl. He is 9 months old. He has no friends around him. He is orange. He lives in a small aquarium, and he like it."
+    prompt_backstorry = "The axolotl salamander has the rare trait of retaining its larval features throughout its adult life. This condition, called neoteny, means it keeps its tadpole-like dorsal fin, which runs almost the length of its body, and its feathery external gills, which protrude from the back of its wide head. Found exclusively in the lake complex of Xochimilco near Mexico City, axolotls differ from most other salamanders in that they live permanently in water. In extremely rare cases, an axolotl will progress to maturity and emerge from the water, but by and large, they are content to stay on the bottom of Xochimilco’s lakes and canals. Axolotls' carnivorous diet historically put them at the top of the food chain. They grab anything they can snatch: Mollusks, fish and arthropods like insects and spiders."
     prompt_dynamic = request.args.get("dynamic")
     prompt_past_conversation = request.args.get("past_conversation")
     prompt_current_question = request.args.get("input_text")
